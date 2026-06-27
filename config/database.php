5f174ec817e952ccee50845b409de60fec83a887
yoloshiku <yoloshiku@gmail.com>
2026-06-27 17:42:42 -0400

    Update database.php

--- config/database.php
+++ config/database.php
@@ -99,24 +99,17 @@ return [
         ],
 
         'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'free-sql-db-3837599'),
-            'username' => env('DB_USERNAME', 'jeremy'),
-            'password' => env('DB_PASSWORD', 'P@ssw0rd'),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
+            'driver'   => 'sqlsrv',
+            'host'     => env('DB_HOST', 'localhost'),
+            'port'     => env('DB_PORT', '1433'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
+            'charset'  => 'utf8',
+            'prefix'   => '',
             'prefix_indexes' => true,
-            'encrypt' => env('DB_ENCRYPT', 'yes'),
-            'login_timeout' => 60,   // wait up to 60s for serverless wake-up
-            'options' => [
-                PDO::ATTR_TIMEOUT => 60,
-            ],
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
-        ],
-
+            'encrypt'  => env('DB_ENCRYPT', 'yes'),
+            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
     ],
 
     /*
