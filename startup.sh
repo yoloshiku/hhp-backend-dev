6b422393853776c379678dabc565373146894baf
yoloshiku <yoloshiku@gmail.com>
2026-06-28 09:15:40 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,6 +10,53 @@ extension=sqlsrv.so
 extension=pdo_sqlsrv.so
 EOINI
 
+# Fix nginx config with correct document root and try_files for Laravel routing
+cat > /etc/nginx/sites-available/default << 'EONGINX'
+server {
+    listen 8080;
+    listen [::]:8080;
+    root /home/site/wwwroot/public;
+    index index.php index.html index.htm;
+    server_name example.com www.example.com;
+    port_in_redirect off;
+
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+
+    error_page 500 502 503 504 /50x.html;
+    location = /50x.html {
+        root /html/;
+    }
+
+    location ~ /\.git {
+        deny all;
+        access_log off;
+        log_not_found off;
+    }
+
+    location ~* [^/]\.php(/|$) {
+        fastcgi_split_path_info ^(.+?\.[Pp][Hh][Pp])(|/.*)$;
+        fastcgi_pass 127.0.0.1:9000;
+        include fastcgi_params;
+        fastcgi_param HTTP_PROXY "";
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_param PATH_INFO $fastcgi_path_info;
+        fastcgi_param QUERY_STRING $query_string;
+        fastcgi_intercept_errors on;
+        fastcgi_connect_timeout         300;
+        fastcgi_send_timeout           3600;
+        fastcgi_read_timeout           3600;
+        fastcgi_buffer_size 128k;
+        fastcgi_buffers 4 256k;
+        fastcgi_busy_buffers_size 256k;
+        fastcgi_temp_file_write_size 256k;
+    }
+}
+EONGINX
+
+service nginx reload
+
 cd /home/site/wwwroot
 
 # Ensure writable dirs exist
